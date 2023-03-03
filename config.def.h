c339eb03664a978d654e3f045561e94b6031183c
charlie foulk <charlesfoulk@csus.edu>
2023-03-02 17:03:31 -0800

    font awesome

--- config.def.h
+++ config.def.h
@@ -7,11 +7,11 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14", "NotoColorEmoji:size=14"};
+static const char *fonts[]          = { "monospace:size=14", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true", "FontAwesome:pixelsize=14:antialias=true:autohint=true"};
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
